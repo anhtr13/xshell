1984062d0e73e24f76ffad9d85f781f805aaee6c
anhtr13 <anhtr13@proton.me>
2026-02-03 16:06:28 +0700

    handle invalid command

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut cmd = String::new();
+    io::stdin().read_line(&mut cmd).unwrap();
+    if cmd != "" {
+        eprintln!("{cmd}: command not found");
+        return;
+    }
 }
